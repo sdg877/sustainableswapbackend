dfa4a2fd2ed1fb802378f544617f7073c662ae7a
Sdg877 <sdrakegill@gmail.com>
2024-03-01 14:38:08 +0000

    final updates

--- backend/settings.py
+++ backend/settings.py
@@ -190,8 +190,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+     'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
+     'REFRESH_TOKEN_LIFETIME': timedelta(days=4),
      'ROTATE_REFRESH_TOKENS': True,
      'BLACKLIST_AFTER_ROTATION': True
 }
