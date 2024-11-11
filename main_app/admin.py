797ec1b256b90fef546afe1b8c85b61fae7ebd89
Sdg877 <sdrakegill@gmail.com>
2024-11-11 06:40:37 +0000

    Cleaned up nested backend directory

--- main_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Item, Swap, Profile
-
-# Register your models here.
-admin.site.register(Item)
-admin.site.register(Swap)
-admin.site.register(Profile)
