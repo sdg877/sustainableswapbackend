f05ff343c083ab219f5cd8fc3973783278dd49ea
Sdg877 <sdrakegill@gmail.com>
2024-11-12 11:23:21 +0000

    Update to asgi file

--- asgi.py
+++ asgi.py
@@ -6,12 +6,10 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-# backend/asgi.py
 import os
+from django.core.asgi import get_asgi_application
 
-def application():
-  from django.core.asgi import get_asgi_application
-  # Replace 'myproject' with your project name
-  os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-  application = get_asgi_application()
-  return application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+application = get_asgi_application()
+
